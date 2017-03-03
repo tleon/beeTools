a1f0c14181519f4bf45698befa516a75cd457e14
slivo-fr <gchopelet@gmail.com>
2017-03-04 00:19:18 +0100

    Boot alarm

--- AsyncTask/app/src/main/java/ragus/lienty/asynctask/MainActivity.java
+++ AsyncTask/app/src/main/java/ragus/lienty/asynctask/MainActivity.java
@@ -1,22 +1,8 @@
 package ragus.lienty.asynctask;
 
-import android.app.ActivityManager;
-import android.app.AlarmManager;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,11 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent serviceIntent = new Intent(this,ServiceApi.class);
-        serviceIntent.putExtra("keyId","");
-        serviceIntent.putExtra("vCode","");
-        startService(serviceIntent);
 
         AlarmRegister.setAlarm(this);
     }
