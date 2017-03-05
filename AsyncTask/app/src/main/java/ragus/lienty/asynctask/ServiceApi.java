bc9e29c0ba873d39fb449d580f2f2f77ba33218a
slivo-fr <gchopelet@gmail.com>
2017-03-05 19:32:38 +0100

    Add notification sound

--- AsyncTask/app/src/main/java/ragus/lienty/asynctask/ServiceApi.java
+++ AsyncTask/app/src/main/java/ragus/lienty/asynctask/ServiceApi.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -45,13 +47,15 @@ public class ServiceApi extends IntentService {
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, MainActivity.class), 0);
 
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle(msg)
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
-                        .setContentText("From " + senderName);
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                        .setContentText("From " + senderName)
+                        .setSound(alarmSound);
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(Integer.parseInt(notifId), mBuilder.build());
